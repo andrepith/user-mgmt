c8ae537b8f5bb2ed999a9dff25bf150d75f61046
Andre Pith <andrepith@yahoo.co.uk>
2021-02-07 23:08:25 +0700

    Mock data

---
+++ src/services/index.js
@@ -0,0 +1,23 @@
+export const data = [
+  {
+    id: 1,
+    firstName: "John",
+    lastName: "Doe",
+    email: "john.doe@gmail.com",
+    dob: 1595493789,
+  },
+  {
+    id: 2,
+    firstName: "Jane",
+    lastName: "Doe",
+    email: "jane.doe@gmail.com",
+    dob: 1595493789,
+  },
+  {
+    id: 3,
+    firstName: "Dave",
+    lastName: "Ghrol",
+    email: "dave.ghrol@gmail.com",
+    dob: 1595493789,
+  },
+];
