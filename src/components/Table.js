3a4539ee3f49a67a2da12da5344394cda8304bd0
Andre Adikara <andrepith@yahoo.co.uk>
2021-02-21 22:19:47 +0700

    Add table component

---
+++ src/components/Table.js
@@ -0,0 +1,35 @@
+const Table = ({ data, editUser, deleteUser }) => {
+  return (
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th scope="col">First Name</th>
+          <th scope="col">Last Name</th>
+          <th scope="col">Email</th>
+          <th scope="col">Date of Birth</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item, key) => (
+          <tr key={key}>
+            <td>{item.firstName}</td>
+            <td>{item.lastName}</td>
+            <td>{item.email}</td>
+            <td>{item.dob}</td>
+            <td>
+              <div className="btn btn-edit" onClick={editUser(item)}>
+                Edit
+              </div>
+              <div className="btn btn-remove" onClick={deleteUser(item)}>
+                Remove
+              </div>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
