a75f58e0658543851651e3faec4a91dfeac506f4
Andre Adikara <andrepith@yahoo.co.uk>
2021-02-21 22:20:03 +0700

    Style table with bootstrap

--- src/containers/UserManagement.js
+++ src/containers/UserManagement.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import Modal from "react-modal";
-import { uniqBy, sortBy } from "lodash";
+import Table from "../components/Table";
 import UserForm from "../components/Form";
 import { getList, postList, deleteList, patchList } from "../services";
 
@@ -42,23 +42,13 @@ const UserManagement = () => {
   return (
     <>
       <div className="container">
-        <h1>User Management</h1>
-        <button onClick={createUser}>Add new user</button>
-        <div>
-          {userData.map((item, key) => (
-            <div key={key} style={{ display: "flex" }}>
-              <div>
-                <div>
-                  {item.firstName} {item.lastName}
-                </div>
-                <div>{item.email}</div>
-                <div>{item.dob}</div>
-              </div>
-              <button onClick={editUser(item)}>Edit</button>
-              <button onClick={deleteUser(item)}>Remove</button>
-            </div>
-          ))}
+        <div className="table-header">
+          <h1>User Management</h1>
+          <div className="my-auto btn btn-add" onClick={createUser}>
+            Add new user
+          </div>
         </div>
+        <Table data={userData} editUser={editUser} deleteUser={deleteUser} />
       </div>
       <Modal isOpen={showModal} onRequestClose={closeModal}>
         <UserForm
